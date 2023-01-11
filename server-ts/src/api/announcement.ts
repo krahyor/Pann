44982355fb4a80ed4e3c490e5b4d6f0d10545862
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2023-01-11 22:44:11 +0700

    import AuthData ใน announcement

--- server-ts/src/api/announcement.ts
+++ server-ts/src/api/announcement.ts
@@ -1,14 +1,19 @@
 import Router from "koa-router"
 const router = new Router()
 import db from "../db"
+import { AuthData } from "../auth"
 
 router
-    .get('/', async(ctx,next) => {
-        ctx.body = [
-            { id: 1, topic: '240-124 Midterm 1/2566', description: 'คะแนนกลางภาควิชา Web Dev', pubDateTime: '2022-09-08 10:00:00', userCode: 'suthon.s'},
-            { id: 2, topic: 'ทุนเรียนดีประจำปี 2566', description: 'test', remarkIfPositive: 'ขอแสดงความยินดีกับผุ้ได้รับทุนเรียนดีทุกคนด้วย', pubDateTime: '2022-09-09 15:00:00', userCode: 'suthon.s'}
-        ]
-    })
+  .get('/', async (ctx, next) => {            
+    const authData = ctx.state.authData as AuthData
+    let query = db('announcement').select('*').where({ 'announcement.userCode': authData.username })
+    if (ctx.request.query['keyword']) {
+      const keyword = String(ctx.request.query['keyword'])
+      query = query.where((it) => {it.where('topic', 'like', `%${keyword}%`).orWhere('description', 'like', `%${keyword}%`)})
+    }
+    ctx.body = await query.orderBy('id', 'desc')
+  })
+
 
 export default router
 
