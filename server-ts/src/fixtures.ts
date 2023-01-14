63d25aa7772a007df3dc42f76eab3052f9d562f8
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2023-01-14 22:43:34 +0700

    แก้ไข fixtures

--- server-ts/src/fixtures.ts
+++ server-ts/src/fixtures.ts
@@ -15,7 +15,7 @@ async function loadFixtures(clearData = false){
   await db.batchInsert('userResult', [
     { id: 1, announcementId: 1, result: '24.5', remark: 'โดนหักคะแนนจากการเข้าสอบสาย', updateDateTime: '2022-09-07 09:12:31', userCode: '6210110010'},
     { id: 2, announcementId: 1, result: '30.1', updateDateTime: '2022-09-07 11:12:00', userCode: '6210110227'},
-    { id: 3, announcementId: 2, result: 'ได้รับทุน', remark: '', resultType: 1,  updateDateTime: '2022-09-08 10:12:30', userCode: '6510110218'},
+    { id: 3, announcementId: 2, result: 'ได้รับทุน', remark: '', resultType: 1,  updateDateTime: '2022-09-08 10:12:30', userCode: '6210110227'},
   ])
 }
 
