3a285c2b8b7c313d9992b8fd52070b9867dd1925
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2023-01-17 18:51:34 +0700

    fix index in repositories

--- store-client/src/repositories/index.ts
+++ store-client/src/repositories/index.ts
@@ -2,7 +2,7 @@ import { AnnouncementRepository } from "./AnnouncementRepository";
 import { UserResultRepository } from "./UesrResultRepository";
 
 const repository = {
-    announcement: new AnnouncementRepository(),
-    UserResult: new UserResultRepository()
+    announcements: new AnnouncementRepository(),
+    UserResults: new UserResultRepository()
 }
 export default repository
