7a2be34f168b2b0d8b1150ed50785fcaae1280a2
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2023-01-18 16:50:06 +0700

    fix router ให้มันขึ้น หน้าเว็บ

--- store-client/src/config/router.tsx
+++ store-client/src/config/router.tsx
@@ -1,39 +1,59 @@
-import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { useEffect } from "react"
+import {Route, Routes, useLocation, Navigate} from "react-router-dom"
+import { useAppCtx } from "../AppProvider"
+import AnnouncementList from "../pages/announcement-list"
 
-import Login from '../pages/login';
-import AnnouncementList from '../pages/announcement-list';
-import UserResultList from '../pages/user-result-list'
-import { useAppCtx } from '../AppProvider';
+import Login from "../pages/login"
+import UserResultList from "../pages/user-result-list"
 
 type Props = {
-  staffOnly?: boolean
-  children: JSX.Element
+    staffOnly?: boolean
+    children: JSX.Element
 }
 
-const ProtectedRoute = ({staffOnly, children }: Props) => {
-  const {userInfo, action} = useAppCtx();
-  const location = useLocation()
-  const staffDenied = staffOnly && !action.isStaff()
-  if (!userInfo.ready || staffDenied) {    
-    if(staffDenied){
-      action.signOut()
-    }
-    console.log('backTo = ', location.pathname)
-    return  <Navigate to="/login" replace state={{backTo: location.pathname}}/>;
-  }
+const ProtectedRoute = ({staffOnly, children}: Props) => {
+    const {userInfo, action} = useAppCtx()
+    const location = useLocation()
 
-  return children;
+    const redirectToLogin = () => {
+        action.signOut()
+        console.log('backTo = ', location.pathname)
+        return <Navigate to="/login" replace state={{backTo: location.pathname}}/>
+    }
+
+    const reFresh = () => {
+        console.log('backTo = ', location.pathname)
+        return <Navigate to="/" replace state={{backTo: location.pathname}}/>
+    }
+
+    useEffect(() => {
+        const json = sessionStorage.getItem('userInfo') as string
+        const data = JSON.parse(json)
+        if (!userInfo.ready || (staffOnly && !action.isStaff())) {
+            if(data.ready){
+                if(staffOnly && !data.staff){
+                    redirectToLogin()
+                }else{
+                    reFresh()
+                }
+            }else{
+                redirectToLogin()
+            }          
+        }
+    }, [userInfo.ready, staffOnly])
+
+    return children
 }
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route index element={<Login />} />
-      <Route path="login" element={<Login/>} />
-      <Route path="home" element={<ProtectedRoute><UserResultList/></ProtectedRoute>} />
-      <Route path="announcement" element={<ProtectedRoute><AnnouncementList/></ProtectedRoute>} />
-    </Routes>
-  );
-};
+    return (
+        <Routes>
+            <Route index element={<Login />} />
+            <Route path="login" element={<Login />}/>
+            <Route path="home" element={<ProtectedRoute><UserResultList/></ProtectedRoute>} />
+            <Route path='announcement' element={<ProtectedRoute staffOnly={true}><AnnouncementList/></ProtectedRoute>}/>
+        </Routes>
+    )
+}
 
-export default AppRoutes;
+export default AppRoutes
