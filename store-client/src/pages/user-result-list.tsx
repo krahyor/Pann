abd5d97b0602882c0f04775abefb95d5bf95e283
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2023-01-18 07:44:53 +0700

    fix user-result

--- store-client/src/pages/user-result-list.tsx
+++ store-client/src/pages/user-result-list.tsx
@@ -11,6 +11,10 @@ function UserResultList() {
   const [selectFilter, setSelectFilter] = useState('');
   const [searchFilter, setSearchFilter] = useState('');
 
+
+  const onUpdateUserResult = (userResult: UserResult) => {
+    setUserResultList(prevUserResultList => prevUserResultList.map(item => item.id === userResult.id ? userResult : item))}
+
   const fetchUserResultList = async () => {
     let params: { keyword?: string, isPinned?: boolean } = {}
     if (searchFilter) {
@@ -54,13 +58,13 @@ function UserResultList() {
         </MenuItem>
         <MenuItem value={1}>Pinned</MenuItem>
       </Select>
-      <TextField sx={{ m: 2, minWidth: 120 }} label="Search" variant="outlined" value={searchFilter} onChange={handleChangeSearchFilter} />
+      <TextField sx={{ m: 2, minWidth: 120 }} label="Search" placeholder="Topic, Description" variant="outlined" value={searchFilter} onChange={handleChangeSearchFilter} />
       {userResultList.length
         ?
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10 }}>
           {userResultList.map((userResult, index) =>
             <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
-              <UserResultCard userResult={userResult}></UserResultCard>
+              <UserResultCard userResult={userResult} onUpdateUserResult={onUpdateUserResult}></UserResultCard>
             </Grid>
           )}
         </Grid>
@@ -73,3 +77,5 @@ function UserResultList() {
     </Box>
   );
 }
+
+export default UserResultList;
