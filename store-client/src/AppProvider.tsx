a0c27787e0b8e9096859c8c696863267111bad46
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2023-01-18 16:51:25 +0700

    fix Appprovider and pann-app-bar

--- store-client/src/AppProvider.tsx
+++ store-client/src/AppProvider.tsx
@@ -1,73 +1,88 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from 'react-oidc-context';
 
 function createCtx<A extends {} | null>() {
-  const ctx = createContext<A | undefined>(undefined);
-  function useCtx() {
-    const c = useContext(ctx);
-    if (c === undefined)
-      throw new Error("useCtx must be inside a Provider with a value");
-    return c;
-  }
-  return [useCtx, ctx.Provider] as const; // 'as const' makes TypeScript infer a tuple
+    const ctx = createContext<A | undefined>(undefined)
+    function useCtx() {
+        const c = useContext(ctx)
+        if (c === undefined)
+            throw new Error('useCtx must be inside a Provider with a value')
+        return c
+    }
+    return [useCtx, ctx.Provider] as const
 }
 
-export interface IAppCtx {
-  userInfo: UserInfo
-  action: {
-    setUserInfo: (userInfo: UserInfo) => void
-    isStaff: () => boolean,
-    signOut: () => void,
-  }
+export interface IAppctx {
+    userInfo: UserInfo
+    action: {
+        setUserInfo: (userInfo: UserInfo) => void
+        isStaff: () => boolean,
+        signOut: () => void,
+    }
 }
 
-export const [useAppCtx, AppCtxProvider] = createCtx<IAppCtx>()
-
+export const [useAppCtx, AppCtxProvider] = createCtx<IAppctx>()
 
 export type UserInfo = {
-  ready: boolean
-  username?: string
-  displayName?: string
-  groups?: string[]
+    ready: boolean
+    username?: string
+    displayName?: string
+    staff?: boolean
 }
 
 const initialUserInfo = {
-  ready: false
+    ready: false
 }
 
 export type AppProviderProps = {
-  children: JSX.Element
+    children: JSX.Element
 }
 
 function AppProvider({children}: AppProviderProps){
-  const auth = useAuth()
-  const [_userInfo, _setUserInfo] = useState<UserInfo>(initialUserInfo)
+    const auth = useAuth()
+    const [_userInfo, _setUserInfo] = useState<UserInfo>(initialUserInfo)
 
-  function setUserInfo(userInfo: UserInfo){
-    _setUserInfo({...userInfo, ready: true})
-  }
+    useEffect(() => {
+        const json = sessionStorage.getItem('userInfo') as string
+        const data = JSON.parse(json)
+        if(data){
+            _setUserInfo({
+                ready: data.ready,
+                username: data.username,
+                displayName: data.displayName,
+                staff: data.staff
+        })}
+    },[])
 
-  function isStaff(){
-    return _userInfo.groups ? _userInfo.groups.indexOf('staff') >= 0 : false
-  }
-  
-  function signOut(){
-    auth.signoutRedirect()    
-    _setUserInfo({ready: false})
-  }
-
-  const appStore = {
-    userInfo: _userInfo,
-    action: {
-      setUserInfo,
-      isStaff,
-      signOut,
+    function setUserInfo(userInfo: UserInfo){
+        _setUserInfo({...userInfo, ready: true,staff:isStaff()})
+        sessionStorage.setItem('userInfo',JSON.stringify(_userInfo))
     }
-  }
+    
+    function isStaff(){
+        const groups:any = auth.user?.profile.groups
+        _userInfo.staff = groups.indexOf('staff') >= 0 ? true:false
+        return _userInfo.staff
+    }
 
-  return (
-    <AppCtxProvider value={appStore}>{children}</AppCtxProvider>
-  )
+    function signOut(){
+        sessionStorage.removeItem('userInfo')
+        auth.signoutRedirect()
+        _setUserInfo({ready: false})
+    }
+
+    const appStore = {
+        userInfo: _userInfo,
+        action: {
+            setUserInfo,
+            isStaff,
+            signOut
+        }
+    }
+
+    return (
+        <AppCtxProvider value={appStore}>{children}</AppCtxProvider>
+    )
 }
 
 export default AppProvider
